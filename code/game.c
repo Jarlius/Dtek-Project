dab67e12f3ddea58a39996c9d8d5cf1851c5c7e0
lortisen <jarl.silven@gmail.com>
2016-12-05 20:10:37 +0100

    :hammer: Added better labworks with working interrupts

--- code/game.c
+++ code/game.c
@@ -14,20 +14,60 @@
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "headers.h"  /* Declatations for these labs */
 
+int mytime = 0;
+int timeoutcount = 0;
+char textstring[] = "text, more text, and even more text!";
+
 /* Interrupt Service Routine */
 void user_isr( void )
 {
-	return;
+	// Switch
+	if((IFS(0) & 0x800) == 0x800) {
+		mytime += 0x3;
+		IFSCLR(0) = 0x800;
+	}
+	// Timer
+	if((IFS(0) & 0x100) == 0x100)
+	{ 
+		IFSCLR(0) = 0x00000100; // Clear the timer interrupt status flag
+
+		if(timeoutcount < 10) {
+			timeoutcount++;
+			return;
+		}
+		timeoutcount = 0;
+		mytime++;
+	}
 }
 
 /* Lab-specific initialization goes here */
-void labinit( void )
+void gameinit( void )
 {
-	return;
+	// Initialize timer-flag
+	IFSCLR(0) = 0x00000100; // Clear the timer interrupt status flag
+	IECSET(0) = 0x00000100; // Enable timer interrupts
+	IPCSET(2) = 0x0000001C; // Set priority level = 7
+	IPCSET(2) = 0x00000003; // Set subpriority level = 3
+	
+	// Initialize switch-flag
+	TRISD |= 0x200;			// Set sw2 to input - testad
+	IFSCLR(0) = 0x800; 		//11 biten	//flagga
+	IECSET(0) = 0x800; 		//11 biten	//enable switch interrupts
+	IPCSET(2) = 0x18000000;	//28:26	//mainprio 6
+	IPCSET(2) = 0x2000000;	//25:24	//subprio 2
+
+	// Initialize timer
+	TMR2 = 0x0;			// Clear the timer register
+	PR2 = 31250;		// sets the roof the tmr2 counts towards before resetting.
+	T2CON |= 0x8000;	// start the timer
+	T2CON |= 0x70;		// sets the scaling to 256
+	
+	enable_interrupt();
 }
 
 /* This function is called repetitively from the main program */
-void labwork( void )
+void gamerun( void )
 {
-	return;
+	display_string(0,itoaconv(mytime));
+	display_update();
 }
--- code/main.c
+++ code/main.c
@@ -1,20 +1,20 @@
-/* projekt
+/* mipslabmain.c
 
    This file written 2015 by Axel Isaksson,
    modified 2015 by F Lundevall
 
    Latest update 2015-08-28 by F Lundevall
 
-   For copyright and licensing, see file COPYING
-*/
+   For copyright and licensing, see file COPYING */
+
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "headers.h"  /* Declatations for these labs */
 
 int main(void) {
 	/* Set up peripheral bus clock */
-    /* OSCCONbits.PBDIV = 1; */
-    OSCCONCLR = 0x100000; /* clear PBDIV bit 1 */
+	/* OSCCONbits.PBDIV = 1; */
+	OSCCONCLR = 0x100000; /* clear PBDIV bit 1 */
 	OSCCONSET = 0x080000; /* set PBDIV bit 0 */
 	
 	/* Set up output pins */
@@ -41,7 +41,7 @@ int main(void) {
 	/* SPI2STAT bit SPIROV = 0; */
 	SPI2STATCLR = 0x40;
 	/* SPI2CON bit CKP = 1; */
-    SPI2CONSET = 0x40;
+	SPI2CONSET = 0x40;
 	/* SPI2CON bit MSTEN = 1; */
 	SPI2CONSET = 0x20;
 	/* SPI2CON bit ON = 1; */
@@ -55,12 +55,12 @@ int main(void) {
 	display_update();
 	
 	display_image(96, icon);
-	
-	labinit(); /* Do any lab-specific initialization */
+
+	gameinit(); /* Do any lab-specific initialization */
 
 	while( 1 )
 	{
-		labwork(); /* Do lab-specific things again and again */
+		gamerun(); /* Do lab-specific things again and again */
 	}
 	return 0;
 }
